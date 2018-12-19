a5883e559f4c24989c8f97989e899f29a8611056
[lakshay] <[laksh1013shar@gmail.com]>
2018-12-19 16:35:27 +0530

    Webapack demo basic code

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+//console.log("hello");
+//const x=10;
+//console.log(x);
+
+//let arr =[1,2,3];
+//console.log(arr);
+
+const App =() => <h1>Webpack demo with react</h1>
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = 
+{
+  entry: './src/index.js',
+  output: 
+  {
+    path:path.join(__dirname,'build'),
+    filename:'bundle.js'
+  },
+  module:
+  {
+    	rules: 
+    	[
+      		{ 
+      			test: /.js$/,
+      			exclude:/(node_modules)/,
+      		  	use:
+      		  	{
+      		  		loader:'babel-loader',
+      		  		options:
+      		  		{
+      		  			presets:['env','react']
+      		  		}
+      		  	} 
+      		}
+    	]
+  }
+
+
+
+
+
+
+
+
+
+
+
+};
